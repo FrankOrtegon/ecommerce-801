2fda5c637686ae4a9af1434410a59d45d355d763
Frank Ortegon Palomino <frank.ortegon@sofka.local>
2025-11-14 08:07:48 -0500

    Se agrega configuración de bean de AWS

--- auth/src/main/java/com/ecommerce/auth/application/config/UseCaseConfig.java
+++ auth/src/main/java/com/ecommerce/auth/application/config/UseCaseConfig.java
@@ -1,6 +1,7 @@
 package com.ecommerce.auth.application.config;
 
 import com.ecommerce.auth.domain.model.gateway.EncrypterGateway;
+import com.ecommerce.auth.domain.model.gateway.NotificationGateway;
 import com.ecommerce.auth.domain.model.gateway.UsuarioGateway;
 import com.ecommerce.auth.domain.usecase.UsuarioUseCase;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class UseCaseConfig {
 
     @Bean
-    public UsuarioUseCase usuarioUseCase(UsuarioGateway usuarioGateway, EncrypterGateway encrypterGateway){
-        return new UsuarioUseCase(usuarioGateway,encrypterGateway);
+    public UsuarioUseCase usuarioUseCase(UsuarioGateway usuarioGateway, EncrypterGateway encrypterGateway, NotificationGateway notificationGateway){
+        return new UsuarioUseCase(usuarioGateway,encrypterGateway, notificationGateway);
     }
 }
